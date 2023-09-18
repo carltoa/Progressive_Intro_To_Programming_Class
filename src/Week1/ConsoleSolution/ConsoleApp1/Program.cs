538b00e71dd5ced442a982c3873a18351d1cf495
Carlton Allen <carltoa@bgsu.edu>
2023-09-18 16:25:50 -0400

    Day 1 code

---
+++ src/Week1/ConsoleSolution/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        string message = "Welcome to Class";
+
+        DateTime now = DateTime.Now;
+
+        string finalMessage = $"The message {message} and it is now {now:t}";
+
+
+        var app = builder.Build();
+
+        app.MapGet("/message", () =>
+        {
+            var response = new MessageResponseModel("this is an API! Wow!", DateTime.Now);
+            return Results.Ok(finalMessage);
+        });
+
+        app.Run();
+    }
+}
+
+public record MessageResponseModel(string Message, DateTime When);
