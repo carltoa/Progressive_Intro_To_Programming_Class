a2056437bc7a396f9353b5ea7d27df6bafceab60
Carlton Allen <carltoa@bgsu.edu>
2023-09-19 11:11:28 -0400

    Day 2: implicitly and explicityly typed local variables

---
+++ src/CSharpNotesSolution/CSharpNotes/Types/DeclaringLocalVariables.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharpNotes.Types;
+
+public class DeclaringLocalVariables
+{
+    [Fact]
+    public void ExplicitlyTypedLocalVariable()
+    {
+        int a = 0;
+        int b = 2;
+
+        string dogName = "Rover";
+
+        Assert.Equal("Rover", dogName);
+
+        Assert.Equal(0, a);
+        Assert.Equal(2, b);
+    }
+
+    [Fact]
+    public void ImplicitlyTypedLocalVariablesWithVar()
+    {
+        var a = 0;
+        var b = 1.0; //defaults to a double
+        var c = "Cat";
+        var d = 'A';
+        var e = true;
+        decimal salary = 80123.23M; //specifically a type with exactly 2 decimal places -- declare by putting "M" after the number
+        float floatingPoint = 12345.1244F; //defining floating point instead of double requires "F" after the number
+
+        var rover = new Dog(); //Dog class defined at bottom of this file
+    }
+}
+
+public class Dog { }
