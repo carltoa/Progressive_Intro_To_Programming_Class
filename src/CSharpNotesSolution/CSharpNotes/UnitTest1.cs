2c07843eb97826d81422d02b17a54026abfd205e
Carlton Allen <carltoa@bgsu.edu>
2023-09-19 10:29:27 -0400

    Day 2: xUnit test framework intro

---
+++ src/CSharpNotesSolution/CSharpNotes/UnitTest1.cs
@@ -0,0 +1,24 @@
+namespace CSharpNotes;
+
+public class UnitTest1
+{
+    [Fact] //attribute
+    public void CanAddTwoSpecificIntegers()
+    {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        Assert.Equal(30, sum);
+    }
+
+    [Theory]
+    [InlineData(10, 20, 30)]
+    [InlineData(2, 2, 4)]
+    [InlineData(10, 2, 12)]
+    public void GivenAnyTwoIntegersWeCanAddThemTogether(int a, int b, int expected)
+    {
+        int sum = a + b;
+        Assert.Equal(expected, sum);
+    }
+}
