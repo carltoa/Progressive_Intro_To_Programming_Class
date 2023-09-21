6c75faf89cc409dbe08f3032e1fb8da1908746c0
Carlton Allen <carltoa@bgsu.edu>
2023-09-21 16:38:49 -0400

    Day 3: Finished Banking Account project; did StringCalculator dependency injection

---
+++ src/Week1/stringcalculator-Thurs-9-21/StringCalculatorInteractionTests.cs
@@ -0,0 +1,35 @@
+﻿using NSubstitute;
+
+namespace StringCalculator;
+public class StringCalculatorInteractionTests
+{
+
+    [Theory]
+    [InlineData("9", "9")]
+    [InlineData("10", "10")]
+    public void ResultsAreWrittenToALogger(string numbers, string logged)
+    {
+        var fakeLogger = Substitute.For<ILogger>();
+        var fakeWebService = Substitute.For<IWebService>();
+        var calculator = new StringCalculator(fakeLogger, fakeWebService);
+
+        calculator.Add(numbers);
+
+        fakeLogger.Received(1).Write(logged);
+        fakeWebService.DidNotReceive().NotifyOfLoggerFailure(Arg.Any<string>());
+    }
+
+    [Fact]
+    public void WebServiceCalledIfLoggerThrows()
+    {
+        var fakeLogger = Substitute.For<ILogger>();
+        var fakeWebService = Substitute.For<IWebService>();
+        fakeLogger.When(c => c.Write(Arg.Any<string>())).Throw<LoggerException>();
+        var calculator = new StringCalculator(fakeLogger, fakeWebService);
+
+        calculator.Add("123");
+
+        fakeWebService.Received(1).NotifyOfLoggerFailure("Logger Went Boom");
+
+    }
+}
---
+++ src/Week1/stringcalculator-Thurs-9-21/StringCalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using NSubstitute;
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    private readonly StringCalculator calculator;
+
+    public StringCalculatorTests()
+    {
+        calculator = new StringCalculator(Substitute.For<ILogger>(), Substitute.For<IWebService>());
+    }
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void ArbitraryNumbers()
+    {
+        var result = calculator.Add("1,2,3,4,5,6,7,8,9");
+
+        Assert.Equal(45, result);
+    }
+}
